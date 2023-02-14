8dd1afca5d3c91ede180d5482a12a36de21f9140
🥷 Pedro Arruda <pedrohendp@gmail.com>
2023-02-13 22:53:17 -0300

    1 commit principal

---
+++ Scripts - Python/teste01.py
@@ -0,0 +1,4 @@
+nome = input('Qual é o seu nome ? ')
+idade = input('Qual é a sua idade ? ')
+peso = input('Qual é o seu peso ? ')
+print('Olá ', nome, 'Tem ', idade , 'Anos', 'E Pesa ', peso, ' KG')
